b071b56063830ead7d08d4a53099b7173d4a1543
kamera <asahi20000124@icloud.com>
2017-07-31 12:27:27 +0900

    カメラモードの時に枠の画像が表示されるようにした

--- Assets/Kamera/Script/CameraMode.cs
+++ Assets/Kamera/Script/CameraMode.cs
@@ -8,24 +8,32 @@ public class CameraMode : MonoBehaviour {
     private Animator animator;
     public Camera firstPersonCamera;
     public Camera thirdPersonCamera;
+    public Canvas canvas;
     private Vector3 vector3Idlerotation;
+
     // Use this for initialization
     void Start () {
         animator = GetComponent<Animator>();
 	}
     // Update is called once per frame
     void Update () {
+        vector3Idlerotation = transform.rotation.eulerAngles;
+        //Debug.Log(vector3Idlerotation.x);
         if (Input.GetKeyDown("z")) {
             ModeCameraChange();
         }
         if (cameraMode) {
             animator.SetBool("Run", false);
             animator.SetBool("Back", false);
-            if (Input.GetKey("up")) {
-                transform.Rotate(new Vector3(-90 * Time.deltaTime, 0f, 0f));
+            if(vector3Idlerotation.x > 320 || vector3Idlerotation.x < 55) {
+                if (Input.GetKey("up")) {
+                    transform.Rotate(new Vector3(-90 * Time.deltaTime, 0f, 0f));
+                }
             }
-            if (Input.GetKey("down")) {
-                transform.Rotate(new Vector3(90 * Time.deltaTime, 0f, 0f));
+            if(vector3Idlerotation.x > 315 || vector3Idlerotation.x < 40) {
+                if (Input.GetKey("down")) {
+                    transform.Rotate(new Vector3(90 * Time.deltaTime, 0f, 0f));
+                }
             }
             if (Input.GetKey("right")) {
                 transform.Rotate(new Vector3(0f, 90 * Time.deltaTime, 0f),Space.World);
@@ -42,6 +50,8 @@ public class CameraMode : MonoBehaviour {
             cameraMode = true;
             firstPersonCamera.enabled = true;
             thirdPersonCamera.enabled = false;
+            canvas.enabled = true;
+
         } 
         else if (GetComponent<PlayerController>().enabled == false) {
             GetComponent<PlayerController>().enabled = true;
@@ -49,7 +59,7 @@ public class CameraMode : MonoBehaviour {
             cameraMode = false;
             firstPersonCamera.enabled = false;
             thirdPersonCamera.enabled = true;
-            vector3Idlerotation = transform.rotation.eulerAngles;
+            canvas.enabled = false;
             transform.rotation = Quaternion.Euler(0f, vector3Idlerotation.y, 0f);
         }
     }
