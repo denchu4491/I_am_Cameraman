bae81cc80b283d67b4d1747c7d09a48c3f713029
kamera <asahi20000124@icloud.com>
2017-08-11 14:54:47 +0900

    重力をPlayerに追加、PlayerHealthスクリプトを追加（こっちは追加のみ）

---
+++ Assets/Kamera/Script/Gravity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+    Rigidbody rb;
+    public float gravity = 3;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.velocity += new Vector3(0.0f,gravity * -1.0f,0.0f);
+	}
+}
---
+++ Assets/Kamera/Script/PlayerHealth.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+    public int HP = 2;
+	// Use this for initialization
+
+}
