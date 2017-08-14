81936b8add42d902a0f1fcb4446b8c55b54d01aa
kamera <asahi20000124@icloud.com>
2017-08-15 01:09:01 +0900

    Timer.csを追加、Timer用のTextをシーンに追加

---
+++ Assets/Kamera/Script/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    public float gameTime;
+    private bool playingGame;
+    void Awake() {
+        
+    }
+	// Use this for initialization
+	void Start () {
+        GameStart();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (playingGame) {
+            gameTime -= Time.deltaTime;
+            GetComponent<Text>().text = ((int)gameTime).ToString();
+            if (gameTime <= 0) {
+                gameTime = 0;
+                GameFinish();
+                playingGame = false;
+            }
+        }
+	}
+    void GameStart() {
+        playingGame = true;
+    }
+    void GameFinish() {
+        Debug.Log("END");
+        //シーン飛ばしそう
+    }
+}
