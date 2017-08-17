33fc45022dc70001884260e78f4e623089094208
Kirby <takasu55220@gmail.com>
2017-08-17 23:49:38 +0900

    いったんコミット

---
+++ Assets/Satrt/script/StartButten.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+ [RequireComponent(typeof(Button))]
+
+public class StartButten : UIBehaviour {
+    
+    protected override void Start() {
+        base.Start();
+
+        GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+
+    void OnClick() {
+        //[GameSelect]シーンに移動
+        SceneManager.LoadScene("GameSelect");
+    }
+}
