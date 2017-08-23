600c616153c8039768f0a6484f352c5bf2c3d92f
Kirby <takasu55220@gmail.com>
2017-08-23 15:01:04 +0900

    コミット

---
+++ Assets/Satrt/script/BoolkeyStart.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class BoolkeyStart : MonoBehaviour {
+
+    public new Animation animation;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Z)) {
+
+            SceneManager.LoadScene("GameSelect");
+           
+
+        } else if (Input.GetKeyDown(KeyCode.X)) {
+            //X入力でチュートリアルスタート
+            SceneManager.LoadScene("Tutorial");
+
+        } else if (Input.GetKeyDown(KeyCode.C)) {
+            //C入力でゲーム終了
+            Application.Quit();
+
+        }
+    }
+   
+   
+}
