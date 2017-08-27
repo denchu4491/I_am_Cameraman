b99da10e8018d1bc4019c740f1c54d6e81937c9e
Kirby <takasu55220@gmail.com>
2017-08-27 11:16:20 +0900

    色々仕掛けを作る途中

---
+++ Assets/Stage/scripts/ThrowRock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowRock : MonoBehaviour {
+
+
+    public GameObject Rock;
+    MeshCollider meshcollider;
+
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.T)) {
+            GameObject obj=Rock;
+            Vector3 t3= GameObject.Find("Button_throw").transform.position;
+            obj.transform.position = t3;
+            obj.AddComponent<Rigidbody>();
+            obj.GetComponent<MeshCollider>().enabled = true;
+            Instantiate(obj);
+        }	
+	}
+}
