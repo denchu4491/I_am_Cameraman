a7917dcbca971a030f0ebf554eca025d4ee8c5d0
Kirby <takasu55220@gmail.com>
2017-08-29 08:00:52 +0900

    撮影した写真を、スコアで表示

---
+++ Assets/Stage/scripts/brokenrocks.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brokenrocks : MonoBehaviour {
+
+
+    Collider collider1;
+
+    void Start() {
+        collider1=GetComponent<Collider>();
+    }
+
+	void Update () {
+        if (gameObject.transform.position.y < 0) {
+            collider1.isTrigger = true;
+        }   
+	}
+
+    void Oncollision() {
+        collider1.isTrigger = true;
+    }
+
+}
