ec7dcced3131b3c9c6e2244b72444c943fe91f45
Kirby <takasu55220@gmail.com>
2017-08-29 13:45:36 +0900

    スクリプトを修正

--- Assets/Stage/scripts/Texture_hey.cs
+++ Assets/Stage/scripts/Texture_hey.cs
@@ -20,7 +20,7 @@ public class Texture_hey : MonoBehaviour {
         
         image = this.GetComponent<Image>();
         path1 = Application.persistentDataPath;
-        texture = PngToTex2D(Application.persistentDataPath + "\\takepicture.png");
+        texture = PngToTex2D(path1 + "\\takepicture.png");
         // Texture -> Spriteに変換する
         Sprite texture_sprite = Sprite.Create(texture, new Rect(0, 0, 1920, 1200), Vector2.zero);
         image.sprite = texture_sprite;
